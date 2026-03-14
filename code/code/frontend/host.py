114ffa3da19cd6334230055bef8e58d5fafc05f5
Mark4572 <Mark4572.mail@gmail.com>
2026-03-14 12:44:36 +0100

    Added files via commit

---
+++ code/code/frontend/host.py
@@ -0,0 +1,55 @@
+import os
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'admin')
+
+@app.route('/')
+def home():
+    return send_file(os.path.join(os.path.dirname(__file__), 'index.html'))
+
+@app.route('/dashboard.css')
+def dashboard_css():
+    return send_file(os.path.join(os.path.dirname(__file__), 'dashboard.css'))
+
+@app.route('/dashboard.js')
+def dashboard_js():
+    return send_file(os.path.join(os.path.dirname(__file__), 'dashboard.js'))
+
+@app.route('/cam.jpg')
+def camera_image():
+    return send_file(os.path.join(os.path.dirname(__file__), 'cam.jpg'))
+
+@app.route('/data')
+def data():
+    return send_file(os.path.join(os.path.dirname(__file__), 'data.html'))
+
+@app.route('/config')
+def config():
+    return send_file(os.path.join(os.path.dirname(__file__), 'config.html'))
+
+@app.route('/home')
+def main_home():
+    return send_file(os.path.join(os.path.dirname(__file__), 'home.html'))
+
+@app.route('/home.css')
+def home_css():
+    return send_file(os.path.join(os.path.dirname(__file__), 'home.css'))
+
+
+@app.route('/dashboard.html')
+def dashboard():
+    return send_file(os.path.join(os.path.dirname(__file__), 'dashboard.html'))
+
+@app.route('/dev')
+def dev():
+    return send_file(os.path.join(os.path.dirname(__file__), 'dev.html'))
+
+
+
+if __name__ == "__main__":
+
+    port = int(os.environ.get("PORT", 47000))
+    app.run(host='127.0.0.1', port=port)
